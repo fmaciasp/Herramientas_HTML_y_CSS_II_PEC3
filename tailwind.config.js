4ef5d37c0b6688c01d6e817640e8ce918a35b50a
fmaciasp <fmaciasp@uoc.edu>
2024-01-02 19:21:37 +0100

    arreglo del nombre del archivo postcssrc y tras entender bien Tailwind hago la cabecera

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'tablet': '640px',
+      'laptop': '1024px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+    },
     extend: {},
+    
   },
   plugins: [],
 }
